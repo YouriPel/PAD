f14f0bcc4fe06814088967ce9b4a623e7f932b1f
Kevin Tjin <kevin_tjin@hotmail.com>
2018-04-24 14:26:59 +0200

    Project opgeschoond

---
+++ PAD Prototype/Assets/Scripts/GameStateScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateScript : MonoBehaviour {
+
+	//CODE NOG NETTER, INHERITANCE??
+	[Header("Gamestates")]
+	public GameObject MainMenu;
+	public GameObject GameScreen;
+	public GameObject EndScreen;
+
+	void Start () {
+		MainMenu.SetActive(true);
+		GameScreen.SetActive (false);
+		EndScreen.SetActive (false);
+	}
+
+	//Method for the startbutton
+	public void StartGame() {
+		MainMenu.SetActive(false);
+		GameScreen.SetActive(true);
+	}
+
+	//method for the back to main menu button
+	public void ClickEndGameButton() {
+		EndScreen.SetActive(false);
+		MainMenu.SetActive(true);
+	}
+}
