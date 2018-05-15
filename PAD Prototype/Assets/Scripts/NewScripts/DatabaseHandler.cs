ea9bbe07634c46d5f7ec79f9ace291716e00dd07
NoahSinay <34063290+NoahSinay@users.noreply.github.com>
2018-05-15 13:04:57 +0200

    ADODB DLL

---
+++ PAD Prototype/Assets/Scripts/NewScripts/DatabaseHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using ADODB;
+
+
+public class DatabaseHandler {
+
+    public Recordset DB_RS;
+    public Connection DB_CONN;
+
+    public void MySQLInit()
+    {
+        try
+        {
+            DB_RS = new Recordset();
+            DB_CONN = new Connection();
+        }
+    }
+}
