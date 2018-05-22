0e0c61ee9bf5a067c7a13494595f1c3bf99bbcea
xCrazyMonkey <kevin_tjin@hotmail.com>
2018-05-22 21:31:33 +0200

    Network files toegevoegd

---
+++ PAD Prototype/Assets/Scenes/Network_Items/GameNetworkManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class GameNetworkManager : NetworkManager {
+
+	// Use this for initialization
+	string ip;
+	int port; 
+	void Awake () {
+		Time.timeScale = 1f;
+	}
+
+	public override void OnServerConnect(NetworkConnection connection){
+		print (connection.connectionId+" server Connected!");
+	}
+
+	public override void OnClientConnect(NetworkConnection connection){
+		print (connection.connectionId+" Connected!");
+		if(Network.connections.Length == 0){
+			ip = Network.player.ipAddress;
+			port = Network.player.port;
+		}
+		else{
+			Network.Connect (ip, port);
+		}
+
+		print ("IP: "+ip +" port: "+port);
+	}
+
+	public override void OnStartHost(){
+		print ("host start");
+	}
+
+	public override void OnStartServer(){
+		print ("server start");
+	}
+}
---
+++ PAD Prototype/Assets/Scenes/Network_Items/SpelerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine;
+using System;
+
+public class SpelerScript : NetworkBehaviour
+{
+    public InputField playerInput;
+	public Button sendButton;
+	//public Text objName;
+	//private GameManagerScript gameManager;
+
+    void Start ()
+	{
+		this.GetComponent<RectTransform> ().localScale = new Vector2 (1, 1);
+		
+		sendButton.onClick.AddListener(SendAnswer);
+    }
+
+	void Update(){
+		if (!isLocalPlayer)
+			return;
+	}
+
+	public void SendAnswer()
+	{
+		if (isLocalPlayer) {
+			print ("local player");
+		}
+		this.gameObject.transform.parent.GetComponent<PlayerObjectScript> ().CmdSetAnswer (playerInput.text);
+		Debug.Log ("tekst: "+playerInput.text);
+	}
+
+    
+}
