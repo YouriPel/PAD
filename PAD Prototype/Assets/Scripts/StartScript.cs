d97db779f0431836098c54407a96b50e624fdac8
TXAI\txai1 <txaipereira@gmail.com>
2018-05-22 17:15:57 +0200

    Added "loading" animation

---
+++ PAD Prototype/Assets/Scripts/StartScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartScript : MonoBehaviour
+{
+    /*public Image logo;
+    public GameObject spelNaam;
+    public Button startbutton;*/
+
+    public float scalingSpeed;
+    public float opacitySpeed;
+    private float scale;
+    private float opacity;
+
+    public bool scaling;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        changeScale();
+        changeOpacity();
+    }
+
+    void changeScale()
+    {
+        if (GetComponent<Image>() == true && scaling == true)
+        {
+            //Changes scale
+            scale += scalingSpeed;
+            transform.localScale = new Vector2(scale, scale);
+
+            //If the scale of the logo is 1 is greater then, stop scaling
+            if (scale > 1)
+            {
+                scalingSpeed = 0;
+            }
+        }
+    }
+
+    void changeOpacity()
+    {
+        //Changes opacity
+        opacity += opacitySpeed;
+        if (GetComponent<Image>() == true)
+        {
+            GetComponent<Image>().color = new Color(1, 1, 1, opacity);
+        }
+        else if (GetComponent<Text>() == true)
+        {
+            GetComponentInChildren<Text>().color = new Color(1, 1, 1, opacity);
+        }
+            
+        //If the opacity of the logo is greater then 1, stop changing the opacity
+        if (opacity > 1)
+        {
+            opacitySpeed = 0;
+        }
+    }
+}
