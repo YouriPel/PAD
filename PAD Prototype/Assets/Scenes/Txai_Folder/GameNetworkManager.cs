60e4a995bdde9f1d1dc22e73be3b92ef7cda5370
Kevin Tjin <kevin_tjin@hotmail.com>
2018-05-22 16:27:51 +0200

    versie push network template

--- PAD Prototype/Assets/Scenes/Txai_Folder/GameNetworkManager.cs
+++ PAD Prototype/Assets/Scenes/Txai_Folder/GameNetworkManager.cs
@@ -8,16 +8,35 @@ public class GameNetworkManager : NetworkManager {
 
 	private GameManagerScript gameManager;
 	// Use this for initialization
+	string ip;
+	int port; 
 	void Awake () {
 		gameManager = GameObject.Find ("GameManager").GetComponent<GameManagerScript> ();
 		Time.timeScale = 1f;
 	}
-	
+
+	public override void OnServerConnect(NetworkConnection connection){
+		print (connection.connectionId+" server Connected!");
+	}
+
 	public override void OnClientConnect(NetworkConnection connection){
 		print (connection.connectionId+" Connected!");
-		gameManager.count++;
-		print ("gameManager.count: "+gameManager.count);
-		//GameObject go = Instantiate (playerPrefab);
-		//go.transform.SetParent (GameObject.Find ("Canvas").transform);
+		if(Network.connections.Length == 0){
+			ip = Network.player.ipAddress;
+			port = Network.player.port;
+		}
+		else{
+			Network.Connect (ip, port);
+		}
+
+		print ("IP: "+ip +" port: "+port);
+	}
+
+	public override void OnStartHost(){
+		print ("host start");
+	}
+
+	public override void OnStartServer(){
+		print ("server start");
 	}
 }
