de46543ac4dfe00a6d4535c2162779edb1e3befb
Ivar <Ivar1998Sanders@gmail.com>
2018-05-23 20:31:37 +0200

    Ivar's Scripts Added

---
+++ PAD Prototype/Assets/Scripts/NewScripts/PlayerID.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerID : MonoBehaviour {
+
+    private int playerId;
+	
+    public void SetPlayerId(int playerId) {
+        this.playerId = playerId;
+    }
+
+    public int GetPlayerId() {
+        return playerId;
+    }
+}
