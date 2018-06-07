323b8ab73fae5df1f0e01f9567d6be6424ea44d6
TXAI\txai1 <txaipereira@gmail.com>
2018-06-07 22:07:17 +0200

    Edited Color changing to be more dynamic

--- PAD Prototype/Assets/Scripts/TimerScript.cs
+++ PAD Prototype/Assets/Scripts/TimerScript.cs
@@ -7,51 +7,71 @@ using UnityEngine.UI;
 public class TimerScript : MonoBehaviour
 {
     public GameObject timer;
+    //Speed in seconds
     public float speed;
+    //Size of the timer
     private Vector2 size;
 
     //Colors for the timer
-    public Color color100;
-    public Color color75;
-    public Color color50;
-    public Color color25;
+    public Color colorGreen;
+    public Color colorRed;
 
     void Start ()
     {
-
+        StartCoroutine("ChangeColor");
     }
-	
+
+    //Determines the smoothness of the lerp. Smaller values are smoother.
+    float smoothness = 0.02f; 
+    IEnumerator ChangeColor()
+    {
+        float progress = 0; //This float will serve as the 3rd parameter of the lerp function
+        while(progress < 1)
+        {
+            GetComponent<RawImage>().color = Color.Lerp(colorGreen, colorRed, progress);
+            progress += (smoothness / speed); //The amount of change to apply
+            yield return new WaitForSeconds(smoothness);
+        }
+    }
+
 	void Update ()
+    {
+        ChangeSize();
+    }
+
+    void ChangeSize()
     {
         size = transform.localScale;
 
         //Changes y size 
-        size.y -= Time.deltaTime * speed;
+        size.y -= Time.deltaTime * (speed / 40);
 
         //If size is above 0, continue changing y size
-        if(size.y > 0)
+        if (size.y > 0)
         {
             transform.localScale = size;
         }
 
-        //If y size is below 75% and higher then 50%, change color to color75
-        if (size.y < 0.75 && size.y > 0.5)
-        {
-            GetComponent<RawImage>().color = color75;
-        }
-        //If y size is below 50% and higher then 25%, change color to color50
-        else if (size.y < 0.5 && size.y > 0.25)
-        {
-            GetComponent<RawImage>().color = color50;
-        }
-        //If y size is below 25%, change color to color25
-        else if (size.y < 0.25)
-        {
-            GetComponent<RawImage>().color = color25;
-        }
-        else if (size.y == 0)
-        {
-            GetComponent<RawImage>().color = color100;
-        }
+        #region Old code where the the color didn't change dynamicly
+        ////If y size is below 75% and higher then 50%, change color to color75
+        //if (size.y < 0.75 && size.y > 0.5)
+        //{
+        //    GetComponent<RawImage>().color = color75;
+        //}
+        ////If y size is below 50% and higher then 25%, change color to color50
+        //else if (size.y < 0.5 && size.y > 0.25)
+        //{
+        //    GetComponent<RawImage>().color = color50;
+        //}
+        ////If y size is below 25%, change color to color25
+        //else if (size.y < 0.25)
+        //{
+        //    GetComponent<RawImage>().color = color25;
+        //}
+        //else if (size.y == 0)
+        //{
+        //    GetComponent<RawImage>().color = color100;
+        //}
+        #endregion
     }
 }
