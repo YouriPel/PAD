5639ff3ee1876cdcb2cdfce55c66ea049d2ba72c
Kevin Tjin <kevin_tjin@hotmail.com>
2018-06-12 16:06:59 +0200

    begin transition script gemaakt

---
+++ PAD Prototype/Assets/Scripts/Menu Scripts/transitionEffectScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class transitionEffectScript : MonoBehaviour {
+
+	public bool left;
+
+	RectTransform rectComponent;
+
+	void Start(){
+		rectComponent = this.GetComponent<RectTransform> ();
+	}
+
+	bool move;
+	void Update(){
+		if(move){
+			
+		}	
+	}
+}
