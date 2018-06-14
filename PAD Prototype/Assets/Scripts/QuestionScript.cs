787f381ace748793822049f90ecae257f725b7ed
TXAI\txai1 <txaipereira@gmail.com>
2018-06-14 12:22:26 +0200

    Edited questionScript

--- PAD Prototype/Assets/Scripts/QuestionScript.cs
+++ PAD Prototype/Assets/Scripts/QuestionScript.cs
@@ -18,11 +18,11 @@ public class QuestionScript : MonoBehaviour {
     //Add the questions in the questions list
 	void AddQuestions()
     {
-        questions.Add(" heeft zonneallergie, welke vitamine krijgt ze nu niet binnen?");
-        questions.Add(" is gecrashed met het vliegtuig en is de enige overlevende Hoeveel dagen kan een mens zonder eten?");
-        questions.Add(" drinkt een halve liter bier in de nachtclub. Hoeveel kJ energie krijgt hij hiervan?");
+        questions.Add(" heeft zonneallergie, welke vitamine krijgt hij/zij nu niet binnen?");
+        questions.Add(" heeft een sappige mango. Hoeveel gram suiker bevat zijn/haar mango?");//
+        questions.Add(" drinkt een halve liter bier in de nachtclub. Hoeveel kJ energie krijgt hij/zij hiervan?");
         questions.Add(" krijgt 293 kcal binnen na het eten van een product. Welk product kan dit zijn?");
-        questions.Add(" heeft gisteren een Big Mac gegeten (503 kcal). Hoeveel minuten moet hij/zij hardlopen (12 km/h) om deze weer te verbranden?");
+        questions.Add(" eet drie appels om energie van te krijgen. Wat kan hij/zij ook eten voor dezelfde hoeveelheid energie?");//
     }
 
     //Add the answers in the answers list
@@ -31,33 +31,33 @@ public class QuestionScript : MonoBehaviour {
         answers.Add("Vitamine A");
         answers.Add("Vitamine B");
         answers.Add("Vitamine C");
-        answers.Add("Vitamine D");
-        answers.Add("14 dagen");
-        answers.Add("6 dagen");
-        answers.Add("28 dagen");
-        answers.Add("35 dagen");
+        answers.Add("Vitamine D");//Goed antwoord
+        answers.Add("11,2 gram");
+        answers.Add("13 gram");
+        answers.Add("11 gram");
+        answers.Add("12,2 gram");//Goed antwoord
         answers.Add("625 KJ");
         answers.Add("515 KJ");
         answers.Add("400 KJ");
-        answers.Add("875 KJ");
-        answers.Add("Een Avocado");
-        answers.Add("Een Ananas");
-        answers.Add("Een Appel");
-        answers.Add("Een knakworst");
-        answers.Add("27 minuten");
-        answers.Add("13 minuten");
-        answers.Add("36 minuten");
-        answers.Add("12 minuten");
+        answers.Add("875 KJ");//Goed antwoord
+        answers.Add("Een avocado");
+        answers.Add("Een ananas");
+        answers.Add("Een appel");
+        answers.Add("Een knakworst");//Goed antwoord
+        answers.Add("1 stuk watermeloen");
+        answers.Add("3 sinaasappel");
+        answers.Add("3 mandarijnen");
+        answers.Add("3 peren");//Goed antwoord
     }
 
     //Add the facts in the facts list
     void AddFacts()
     {
-        facts.Add("Als je schaduw langer is dan jezelf, kun je wel naar buiten gaan, maar je huid zal geen vitamine D maken, hoe zonnig het ook is.");
-        facts.Add("Van je zintuigen is je zicht het eerste wat je verliest als je sterft, Je gehoor staat op de laatste plek.");
+        facts.Add("Vitamine D is van belang voor sterke botten.");
+        facts.Add("Het zal je misschien verbazen, maar mango’s kunnen een ontstekingsremmend werken.");
         facts.Add("Het proosten van een glas bier of wijn stamt uit het tijdperk van de Romeinen. Maar waar is deze gewoonte eigenlijk door ontstaan? Blijkbaar wantrouwde men elkaar, doordat veel Romeinen elkaar vergiftigden. Door ruw te proosten werd er een hoop drank uitgewisseld tussen de glazen. Hierdoor was de kans op vergiftiging een stuk kleiner.");
-        facts.Add("Wist je dat het record voor de meeste broodjes worst eten in 10 minuten 68 broodjes is.");
-        facts.Add("Om een centimeter van je taille af te halen moet je 1,8 kilogram vet verbranden. Dat is ongeveer zesduizend calorieën, oftewel zo’n acht uur hardlopen.");
+        facts.Add("Wist je dat een knakworst ongeveer 1/5 van de geadviseerde hoeveelheid eiwitten per dag bevat?");
+        facts.Add("Wist 80% van een appel bestaat uit water?");
 
     }
 }
